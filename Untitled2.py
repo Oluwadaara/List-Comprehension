561d7eb20751a66e8fd7dbfec95fe746da641835
Oluwadaara <89576468+Oluwadaara@users.noreply.github.com>
2021-10-17 18:01:34 +0100

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+outcome = [x**2 for x in range(1,11)]
+#x**2 is the output expression
+#x is the variable
+#range(1,11) is the input sequence
+outcome
+
+
+# In[ ]:
+
+
+
+
